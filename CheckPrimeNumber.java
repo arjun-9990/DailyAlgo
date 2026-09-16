b55421e6e435c98a43ac8e1cca6007f95eb22dc0
Arjun Gaikwad <arjun.gaikwad@metamorphtech.com>
2026-09-16 22:39:31 +0530

    implement 2nd approch for Prime Number:

--- CheckPrimeNumber.java
+++ CheckPrimeNumber.java
@@ -1,8 +1,31 @@
 class CheckPrimeNumber {
 	
 	public static void main(String args[]){
-		int num = 6;
+		int num = 97;
 		checkPrimeNumber(num);
+		checkPrimeNumberSecondApproch(num);
+	}
+	
+	public static void checkPrimeNumberSecondApproch(int num){
+		
+		int count = 0;
+		
+		if(num <= 1){
+			System.out.println(num + " :- Not a prime Number." );
+			return;
+		}
+		
+		for(int i = 1 ; i <= num ; i++){
+			if(num % i == 0){
+				count++;
+			}
+		}
+		if(count == 2){
+			System.out.println(num + " :- is a prime Number.");
+			return;
+		}
+		System.out.println(num + " :- Not a prime Number." );
+			
 	}
 	
 	public static void checkPrimeNumber(int num){
